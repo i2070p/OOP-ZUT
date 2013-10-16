9093ef9fa864d6c4ad615f6a4ffa27883c2bdc84
i2070p <metiv9@gmail.com>
2013-10-16 23:08:00 +0200

    Update Lab3.cpp

--- Lab3.cpp
+++ Lab3.cpp
@@ -9,19 +9,19 @@ using namespace std;
 
 
 class Student {    
-	public: 	
-        Student(string, int, int, int*);
-        Student(Student&);
-        ~Student();
-        int getId();
-        void changeNext(Student*);
-        string toString();
-        Student* Next();
-	private:
-		int studentId, groupId;
-		int* marks;
-		string initial;
-        Student* next; 
+    public: 	
+    	Student(string, int, int, int*);
+    	Student(Student&);
+    	~Student();
+    	int getId();
+    	void changeNext(Student*);
+    	string toString();
+     	Student* Next();
+    private:
+    	int studentId, groupId;
+    	int* marks;
+    	string initial;
+    	Student* next; 
 };
 
 
@@ -30,15 +30,15 @@ class Group {
     	Group (int);
     	Group (Group&);
         ~Group ();  
-		void addStudent(string, int*);       
+	void addStudent(string, int*);       
         void printStudents();             
         void removeStudent(int);
-		void removeStudents();
+	void removeStudents();
         string toString();
         Student* locateStudent(int); 
-	private:
-		int groupId, studentsCount, idMaker;
-		Student *tail, *head; 
+    private:
+	int groupId, studentsCount, idMaker;
+	Student *tail, *head; 
 };
 
 
@@ -62,122 +62,121 @@ int* createRandomMarksArray() {
 int main ()
 {
     srand(time(0));
-	Group *grp1 = new Group(211);
+    Group *grp1 = new Group(211);
     Group *grp2 = new Group(210);
 
-	stringstream s;
-	for (int i=0; i<5; i++) {
-		s.str("");
-		s << (char)(65+rand()%26) << (char)(65+rand()%26);	
-		grp1->addStudent(s.str(),createRandomMarksArray());
-	}
+    stringstream s;
+    for (int i=0; i<5; i++) {
+        s.str("");
+        s << (char)(65+rand()%26) << (char)(65+rand()%26);	
+        grp1->addStudent(s.str(),createRandomMarksArray());
+    }
 
-	grp2->addStudent("SS",createMarksArray(5,5,5,5,5,5,5,5,5,5));
-	grp2->addStudent("DS",createMarksArray(4,5,4,5,4,5,4,5,4,5));
-	grp2->addStudent("RS",createMarksArray(5,3,5,3,5,3,5,3,5,3));	
-	grp2->addStudent("JK",createMarksArray(5,2,5,2,5,2,5,2,5,2));
-	grp2->removeStudent(2);
-	grp2->addStudent("OR",createMarksArray(2,2,2,2,2,2,2,2,2,3));	
-	grp2->removeStudent(1);
-	grp2->addStudent("PO",createMarksArray(3,4,3,5,5,4,3,2,2,3));
-	grp2->addStudent("RS",createMarksArray(3,4,3,5,5,4,3,2,2,3));
-	grp2->removeStudent(6);	
-	grp2->addStudent("LS",createMarksArray(3,4,3,5,5,4,3,2,2,3));
-	Group *grp3 = new Group(*grp2);
-
-	grp1->printStudents();
-	grp2->printStudents();
-	grp3->printStudents();
-
-
+    grp2->addStudent("SS",createMarksArray(5,5,5,5,5,5,5,5,5,5));
+    grp2->addStudent("DS",createMarksArray(4,5,4,5,4,5,4,5,4,5));
+    grp2->addStudent("RS",createMarksArray(5,3,5,3,5,3,5,3,5,3));	
+    grp2->addStudent("JK",createMarksArray(5,2,5,2,5,2,5,2,5,2));
+    grp2->removeStudent(2);
+    grp2->addStudent("OR",createMarksArray(2,2,2,2,2,2,2,2,2,3));	
+    grp2->removeStudent(1);
+    grp2->addStudent("PO",createMarksArray(3,4,3,5,5,4,3,2,2,3));
+    grp2->addStudent("RS",createMarksArray(3,4,3,5,5,4,3,2,2,3));
+    grp2->removeStudent(6);	
+    grp2->addStudent("LS",createMarksArray(3,4,3,5,5,4,3,2,2,3));
+    
+    Group *grp3 = new Group(*grp2);
+    
+    grp1->printStudents();
+    grp2->printStudents();
+    grp3->printStudents();
+    
     return 0;
 }
 
 //*****************STUDENT**********************************************
 
 Student::~Student() { 
-	delete marks; 
+    delete marks; 
 }
 int Student::getId() { 
-	return(studentId); 
+    return(studentId); 
 }
 Student* Student::Next() { 
-	return(next); 
+    return(next); 
 } 
 void Student::changeNext(Student* _student) { 
-	next=_student; 
+    next=_student; 
 }
 
 Student::Student (string _initial, int _studentId, int _groupId, int* _marks): 
 initial(_initial), studentId(_studentId), groupId(_groupId), marks(_marks) { changeNext(NULL); }
 
 string Student::toString(){
-	stringstream s;
-	s << "Student Id: " << studentId << ", Initial: " << initial << ",  Group ID: " << groupId << endl << "Marks: " << endl;
-	for (int i=0; i<marksCount; i++) s << "Mark " << i << ": " << marks[i] << endl; 
-	s << endl;
-	return(s.str());	
+    stringstream s;
+    s << "Student Id: " << studentId << ", Initial: " << initial << ",  Group ID: " << groupId << endl << "Marks: " << endl;
+    for (int i=0; i<marksCount; i++) s << "Mark " << i << ": " << marks[i] << endl; 
+    s << endl;
+    return(s.str());	
 }
 
 Student::Student(Student& src) {
-	initial=src.initial;
-	groupId=src.groupId;
-	studentId=src.studentId;
-	marks=new int[marksCount];
-	for (int i=0; i<marksCount;i++) marks[i]=src.marks[i];	
-	changeNext(NULL);
+    initial=src.initial;
+    groupId=src.groupId;
+    studentId=src.studentId;
+    marks=new int[marksCount];
+    for (int i=0; i<marksCount;i++) marks[i]=src.marks[i];	
+    changeNext(NULL);
 }
 //*****************GROUP************************************************
 
 Group::~Group (){
-	removeStudents();
+    removeStudents();
 }
 void Group::removeStudents() {
-	Student* tmp;
+    Student* tmp;
     for (int i=0; i<studentsCount; i++) {
-    	tmp=head;
+        tmp=head;
     	head=head->Next();
     	tmp->~Student();
-	}
-	studentsCount=0;
+    }
+    studentsCount=0;
 }
 Group::Group(int _groupId): groupId(_groupId) {
-	 head = NULL; 
-	 tail = NULL; 
-	 studentsCount=0; 
-	 idMaker=0;
+    head = NULL; 
+    tail = NULL; 
+    studentsCount=0; 
+    idMaker=0;
 }
 
 Group::Group(Group &source) {
-	groupId=source.groupId;
-	idMaker=source.idMaker;
-	studentsCount=0;
-	Student *ptr=source.head;
-	for (int i=0; i<source.studentsCount; i++) {
-		Student *tmp = new Student(*ptr);	
-		if (studentsCount==0) {
-			head=tmp;
-			tail=tmp;	
-	    } else {
-    		tail->changeNext(tmp);
-			tail=tmp;
+    groupId=source.groupId;
+    idMaker=source.idMaker;
+    studentsCount=0;
+    Student *ptr=source.head;
+    for (int i=0; i<source.studentsCount; i++) {
+        Student *tmp = new Student(*ptr);	
+        if (studentsCount==0) {
+            head=tmp;
+            tail=tmp;	
+        } else {
+            tail->changeNext(tmp);
+            tail=tmp;
     	}
     	studentsCount++;
-		ptr=ptr->Next();
-	}
+	ptr=ptr->Next();
+    }
 }
 
 string Group::toString(){
-	stringstream s;	
-
-   if (studentsCount>0) {
-   	    s << "____________________GROUP: " << groupId << "_________________________" << endl;
-	    s << "Group Id: " << groupId << ", Students count: " << studentsCount << endl << endl;
+    stringstream s;	
+    if (studentsCount>0) {
+        s << "____________________GROUP: " << groupId << "_________________________" << endl;
+        s << "Group Id: " << groupId << ", Students count: " << studentsCount << endl << endl;
     	Student* tmp;
     	tmp=head;
-    	while (tmp!=NULL){
-        	s << tmp->toString() << endl;
-        	tmp=tmp->Next();
+    	while (tmp!=NULL) {
+            s << tmp->toString() << endl;
+            tmp=tmp->Next();
     	}
     } else s << "Group is empty." << endl;
     return(s.str());
@@ -192,41 +191,39 @@ Student* Group::locateStudent (int i){
 
 void Group::addStudent(string _initial, int* _marks){         
     Student *tmp = new Student(_initial, idMaker++, groupId, _marks);
-	if (studentsCount==0) {
-		head=tmp;
-		tail=tmp;	
-		
+    if (studentsCount==0) {
+        head=tmp;
+        tail=tmp;	
     } else {
     	tail->changeNext(tmp);
-		tail=tmp;
+        tail=tmp;
     }
     studentsCount++;
 }
 
 void Group::printStudents(){                 
-	cout << toString();
+    cout << toString();
 }
 
 void Group::removeStudent(int _id){       
     Student *tmp=head;
-	if (_id==head->getId())
+    if (_id==head->getId())
     {
-	 	head=head->Next();
+        head=head->Next();
         tmp->~Student();
-    }else {
+    } else {
         while ((tmp->Next()!=NULL)&&(tmp->Next()->getId()!=_id)) tmp=tmp->Next();    
         if (tmp->Next()->getId()==_id)
         {
-             Student *toRemove=tmp->Next();
-			 if (tmp->Next()->getId()==tail->getId()) {
-			 	tail=tmp;
-			 	tmp->changeNext(NULL);
-			 } else {
-			 	tmp->changeNext(tmp->Next()->Next());
+            Student *toRemove=tmp->Next();
+            if (tmp->Next()->getId()==tail->getId()) {
+                tail=tmp;
+                tmp->changeNext(NULL);
+            } else {
+                tmp->changeNext(tmp->Next()->Next());
              	toRemove->~Student(); 
-			 }
-
+            }
         }
-	}
-	studentsCount--;  
+    }
+    studentsCount--;  
 }
