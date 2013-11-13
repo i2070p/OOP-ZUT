c0b1daba5a02da539ee9eb8cc309f43063fe1c90
i2070p <metiv9@gmail.com>
2013-11-13 17:44:46 +0100

    Create OffensiveObject.cpp

---
+++ Lab7/OffensiveObject.cpp
@@ -0,0 +1,15 @@
+#include "OffensiveObject.h"
+
+OffensiveObject::OffensiveObject(std::string _info,int _hp, int _damage, std::string _txt_shout):
+GameObject(_info,_hp), damage(_damage), txt_shout(_txt_shout) {
+}
+
+OffensiveObject::~OffensiveObject(void) {}
+
+int OffensiveObject::attack() {
+	if (damage<=0) return(0);
+	return(rand()%damage);
+}
+std::string OffensiveObject::shout() {
+  return(txt_shout);
+}
