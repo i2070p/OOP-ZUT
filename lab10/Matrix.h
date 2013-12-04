46c65f796f6a3f778a6c82d81e3d3040cbaacd55
i2070p <metiv9@gmail.com>
2013-12-04 10:03:12 +0100

    Update Matrix.h

--- lab10/Matrix.h
+++ lab10/Matrix.h
@@ -2,6 +2,7 @@
 #include <sstream>
 #include <string.h>
 #include <fstream>
+
 #define _ifor(a) for(int i=0;i<a;i++)
 #define _for(a,b) for(int a=0;a<b;a++)
 
@@ -9,37 +10,48 @@ using namespace std;
 
 class Matrix {
 public:
-	Matrix(){};
-	Matrix(int,int);
-	Matrix(string);
-	~Matrix(){};
-	int getHeight();
-	int getWidth();
-	friend ostream & operator<< (ostream &out, Matrix &src);
-	//double operator() (int column, int row);
-	double getElement(int,int);
-	void setElement(int, int, double);
-	void transpone();
-	void save(string);
-	Matrix* addValue(double);
-	Matrix* add(Matrix*);
-	Matrix* sub(Matrix*);
-	Matrix* mul(Matrix*);
-	string showRow(int);
-	string showColumn(int);
-	string showElement(int,int);
-	string showMatrix();
-	Matrix operator+ (Matrix&);
-	void operator+= (Matrix&);
-    void operator= (Matrix&);
-	Matrix operator* (Matrix&);
+        Matrix(){};
+        Matrix(int,int);
+        Matrix(string);
+        ~Matrix(){};
+        int getHeight();
+        int getWidth();
+        friend ostream & operator<< (ostream &out, Matrix &src);
+        //double operator() (int column, int row);
+        double getElement(int,int);
+        void setElement(int, int, double);
+        void transpone();
+        void save(string);
+        Matrix* addValue(double);
+        Matrix* add(Matrix*);
+        Matrix* sub(Matrix*);
+        Matrix* mul(Matrix*);
+        string showRow(int);
+        string showColumn(int);
+        string showElement(int,int);
+        string showMatrix();
+        Matrix operator+ (Matrix&);
+        Matrix operator+= (Matrix&);
+        Matrix operator*= (Matrix&);
+        Matrix operator= (Matrix&);
+        Matrix operator* (Matrix&);
+        Matrix operator! ();
+		friend void swap(Matrix& m1, Matrix& m2) {
+				using std::swap;
+	swap(m1.height,m2.height);
+	swap(m1.width,m2.width);
+	swap(m1.matrix,m2.matrix);
+		}
+
+
 private:
-	double **matrix;
-	int height, width;
-	string *parser(string);
-	int str2Int(string);
-	double str2Double(string);
-	double** createMatrix(int, int);
-	bool isElementExist(int,int);
-	Matrix* addOrSub(Matrix*, bool);
+        double **matrix;
+        int height, width;
+        string *parser(string);
+        int str2Int(string);
+        double str2Double(string);
+        double** createMatrix(int, int);
+        bool isElementExist(int,int);
+        Matrix* addOrSub(Matrix*, bool);
+		Matrix* reWrite(Matrix *);
 };
